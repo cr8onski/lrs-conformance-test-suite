10b7c24f0b95975224a6295c5af2d47ae728b14f
Andy Creighton <cr8onski@gmail.com>
2017-02-16 15:56:15 -0500

    fixes #152, update comm 2.5 activites resouce

--- test/v1_0_3/H.Communication2.5-ActivitiesResource.js
+++ test/v1_0_3/H.Communication2.5-ActivitiesResource.js
@@ -23,7 +23,7 @@ describe('Activities Resource Requirements (Communication 2.5)', () => {
 /**  XAPI-00252, Communication 2.5 Activities Resource
  * An LRS has an Activities API with endpoint "base IRI" + /activities" (7.5) Implicit (in that it is not named this by the spec)
  */
-    it('An LRS has an Activities Resource with endpoint "base IRI" + /activities" (Communication 2.5, Implicit) **Implicit** (in that it is not named this by the spec)', function () {
+    it('An LRS has an Activities Resource with endpoint "base IRI" + /activities" (Communication 2.5, XAPI-00252', function () {
         var templates = [
             {statement: '{{statements.default}}'}
         ];
@@ -59,15 +59,15 @@ describe('Activities Resource Requirements (Communication 2.5)', () => {
 /**  XAPI-00251, Communication 2.5 Activities Resource
  * An LRS's Activities API upon processing a successful GET request returns 200 OK and the complete Activity Object
  */
-    it('An LRS\'s Activities Resource upon processing a successful GET request returns the complete Activity Object (Communication 2.5.s1)', function () {
+    it('An LRS\'s Activities Resource upon processing a successful GET request returns the complete Activity Object (Communication 2.5.s1, XAPI-00251)', function () {
         var templates = [
             {statement: '{{statements.object_activity}}'},
             {object: '{{activities.default}}'}
         ];
         var data = helper.createFromTemplate(templates);
         var statement = data.statement;
-        statement.object.id = 'http://www.example.com/verify/complete/34534';
-
+        statement.object.id = 'http://www.example.com/verify/complete/' + helper.generateUUID();
+console.log(statement.object.id);
         return helper.sendRequest('post', helper.getEndpointStatements(), undefined, [statement], 200)
         .then(function () {
             var parameters = {
@@ -89,23 +89,6 @@ describe('Activities Resource Requirements (Communication 2.5)', () => {
         return helper.sendRequest('get', helper.getEndpointActivities(), undefined, undefined, 400);
     });
 
-    //Note: tests focusing on type "String" as a parameter are likely to be stricken or reworded before final release.
-    //Also note: using an it over and it nullifies the inner its, consider using a describe.
-    it('An LRS\'s Activities Resource rejects a GET request with "activityId" as a parameter if it is not type "String" with error code 400 Bad Request (format, Communication 2.5.s1.table1.row1)', function () {
-        var invalidTypes = [1, true, { key: 'value'}];
-        invalidTypes.forEach(function (type) {
-            it('Should reject GET with "activityId" with type ' + type, function () {
-                var parameters = helper.buildActivity();
-                // console.log(parameters);
-                parameters.activityId = type;
-                // console.log(parameters);
-                return helper.sendRequest('get', helper.getEndpointActivities(), parameters, undefined, 400).then((res) => {
-                    // console.log('howdy', res.text);
-                });
-            });
-        });
-    });
-
 /**  XAPI-00254, Communication 2.5 Activities Resource
  * The Activity Object must contain all available information about an activity from any statements who target the same “activityId”. For example, LRS accepts two statements each with a different language description of an activity using the exact same “activityId”. The LRS must return both language descriptions when a GET request is made to the Activities endpoint for that “activityId”.
  */
@@ -119,8 +102,9 @@ describe('Activities Resource Requirements (Communication 2.5)', () => {
         var data2 = helper.createFromTemplate(templates);
         var statement = data.statement;
         var statement2 = data2.statement;
-        statement.object.id = 'http://www.example.com/verify/complete/34534100123';
-        statement2.object.id = 'http://www.example.com/verify/complete/34534100123';
+        var uuid = helper.generateUUID();
+        statement.object.id = 'http://www.example.com/verify/complete/' + uuid;
+        statement2.object.id = 'http://www.example.com/verify/complete/' + uuid;
 
         statement2.object.definition.name['fr-FR'] = "réunion";
         delete statement2.object.definition.name['en-US'];
